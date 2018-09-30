1ea24f5060e702200ff089f5143bd5944aa950c2
JoshuaYonathan <JoshuaYonathan@no-reply.github.com>
2018-09-29 22:05:33 -0400

    updates

--- excel/main.py
+++ excel/main.py
@@ -2,26 +2,28 @@ import os
 import win32com.client
 
 class OpenExcel():
-	def __init__(self, filepath, visible=True, save=True):
-		self.filepath = filepath
-		self.visible = visible
-		self.save = save
+    def __init__(self, filepath, visible=True, save=True):
+        self.filepath = filepath
+        self.visible = visible
+        self.save = save
 
-	def __call__(self):
-		self.xlapp = win32com.client.gencache.EnsureDispatch("Excel.Application")
-		self.xlapp.Visible = self.visible
-		self.xlwb = xlapp.Workbooks.Open(os.path.abspath(filepath))
-		return self.xlwb
+    def __call__(self):
+        self.xlapp = win32com.client.gencache.EnsureDispatch("Excel.Application")
+        self.xlapp.Visible = self.visible
+        self.xlwb = xlapp.Workbooks.Open(os.path.abspath(self.filepath))
+        return self.xlwb
 
-	def __enter__(self):
-		self.xlapp = win32com.client.gencache.EnsureDispatch("Excel.Application")
-		self.xlapp.Visible = self.visible
-		self.xlwb = self.xlapp.Workbook.Open(os.path.abspath(filepath))
-		return self.xlwb
+    def __enter__(self):
+        win32com.client.pythoncom.CoInitialize()
+        self.xlapp = win32com.client.gencache.EnsureDispatch("Excel.Application")
+        self.xlapp.Visible = self.visible
+        self.xlwb = self.xlapp.Workbooks.Open(os.path.abspath(self.filepath))
+        return self.xlwb
 
-	def __exit__(self):
-		if self.save: self.xlwb.Save()
-		self.xlwb.Close()
-		self.xlapp.Quit()
-		del self.xlwb
-		del self.xlapp
+    def __exit__(self, exc_type, exc_value, traceback):
+        if self.save: self.xlwb.Save()
+        self.xlwb.Close()
+        self.xlapp.Quit()
+        win32com.client.pythoncom.CoUninitialize()
+        del self.xlwb
+        del self.xlapp
